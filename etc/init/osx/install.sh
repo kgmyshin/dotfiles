3da1cc13a9c766f97a4177a811716314666a44ea
kgmyshin <kgmyshin82@gmail.com>
2021-05-24 15:46:17 +0900

    macの設定

---
+++ etc/init/osx/install.sh
@@ -0,0 +1,20 @@
+THIS_DIR=$(cd $(dirname $0); pwd)
+pushd THIS_DIR
+
+echo "installing Homebrew ..."
+type "brew" > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo "run brew doctor ..."
+brew doctor
+
+echo "run brew update ..."
+brew update
+
+echo "ok. run brew upgrade ..."
+brew upgrade
+
+brew bundle
+
+brew cleanup
+
+popd
