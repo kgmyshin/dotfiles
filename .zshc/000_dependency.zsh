72d0bbf958df1485b78e2bac6f72724fc3037e96
kgmyshin <kgmyshin82@gmail.com>
2021-05-26 11:28:56 +0900

    hub系コマンドを便利に

--- .zshc/000_dependency.zsh
+++ .zshc/000_dependency.zsh
@@ -6,6 +6,10 @@ if ! (type "ghq" > /dev/null 2>&1) ; then
     echo "ghqをインストールする必要があります"
 fi
 
+if ! (type "hub" > /dev/null 2>&1) ; then
+    echo "hubをインストールする必要があります"
+fi
+
 if ! (type "studio" > /dev/null 2>&1) ; then
     echo "Android Studioをインストールする必要があります。" # launcherの設定も必要な旨を書く
 fi
