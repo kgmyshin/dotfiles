634987ab0b2e4d9110e4a51e3c566c7bbe0e364d
kgmyshin <kgmyshin82@gmail.com>
2021-05-30 17:32:42 +0900

    opendではなくてopeng

--- .zshc/031_alias_ghq.zsh
+++ .zshc/031_alias_ghq.zsh
@@ -9,7 +9,7 @@ if is_osx ; then
   alias asg='studio $(ghq list --full-path | peco)'
   alias codeg='code $(ghq list --full-path | peco)'
   alias cdg='cd $(ghq list --full-path | peco)'
-  alias opend='open $(ghq list --full-path | peco)'
+  alias openg='open $(ghq list --full-path | peco)'
 elif is_linux ; then;
   alias asg='studio $(ghq list --full-path | peco)'
   alias codeg='code $(ghq list --full-path | peco)'
@@ -18,5 +18,5 @@ elif is_win ; then;
   alias asg="ghq list --full-path | peco | xargs wslpath -m | { xargs zsh -ic 'studio \$0' &> /dev/null & }"
   alias codeg='code "$(ghq list --full-path | peco | xargs wslpath -w)"'
   alias cdg='cd $(ghq list --full-path | peco)'
-  alias opend='open $(ghq list --full-path | peco | xargs wslpath -w )'
+  alias openg='open $(ghq list --full-path | peco | xargs wslpath -w )'
 fi
