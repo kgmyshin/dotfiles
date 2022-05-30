182e28b06b791aa9b1bc10c6a476a989135edf25
Shinnosuke Kugimiya <kgmyshin82@gmail.com>
2022-05-30 14:01:53 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,18 +7,10 @@ has() {
 }
 
 if [ ! -d ${DOT_DIR} ]; then
-    if has "curl" || has "wget"; then
-        TARBALL="https://github.com/kgmyshin/dotfiles/archive/refs/heads/main.tar.gz"
-        if has "curl"; then
-            curl -L ${TARBALL} -o main.tar.gz
-        else
-            wget ${TARBALL}
-        fi
-        tar -zxvf main.tar.gz
-        rm -f main.tar.gz
-        mv -f dotfiles-main "${DOT_DIR}"
+    if has "git"; then
+        git clone https://github.com/kgmyshin/dotfiles.git ${DOT_DIR}
     else
-        echo "curl or wget or git required"
+        echo "git required"
         exit 1
     fi
 
