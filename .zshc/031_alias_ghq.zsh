f491b5a935111fe61d29a75d09971c6af368bede
kgmyshin <kgmyshin82@gmail.com>
2022-05-30 16:14:41 +0900

    dependency_checkは不要

--- .zshc/031_alias_ghq.zsh
+++ .zshc/031_alias_ghq.zsh
@@ -6,16 +6,13 @@
 alias hubg='hub browse $(ghq list -e | peco | sed -e "s/github.com\///")'
 
 if is_osx ; then
-  alias asg='studio $(ghq list --full-path | peco)'
   alias codeg='code $(ghq list --full-path | peco)'
   alias cdg='cd $(ghq list --full-path | peco)'
   alias openg='open $(ghq list --full-path | peco)'
 elif is_linux ; then;
-  alias asg='studio $(ghq list --full-path | peco)'
   alias codeg='code $(ghq list --full-path | peco)'
   alias cdg='cd $(ghq list --full-path | peco)'
 elif is_win ; then;
-  alias asg="ghq list --full-path | peco | xargs wslpath -m | { xargs zsh -ic 'studio \$0' &> /dev/null & }"
   alias codeg='code "$(ghq list --full-path | peco | xargs wslpath -w)"'
   alias cdg='cd $(ghq list --full-path | peco)'
   alias openg='open $(ghq list --full-path | peco | xargs wslpath -w )'
