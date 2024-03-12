124dc310358376030ed401865f1e42fce29c1a7b
kgmyshin <kgmyshin82@gmail.com>
2024-03-12 10:49:01 +0900

    update zsinit

--- .zshc/003_zinit.zsh
+++ .zshc/003_zinit.zsh
@@ -1,11 +1,9 @@
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 
-if ! (type "zinit" > /dev/null 2>&1) ; then
-	mkdir -p "$(dirname $ZINIT_HOME)"
-	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-fi
-
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
+
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
@@ -18,7 +16,6 @@ zinit load junegunn/fzf-bin
 zinit snippet OMZL::git.zsh
 
 zinit snippet OMZP::git
-zinit snippet OMZP::flutter
 zinit snippet OMZP::tig
 zinit snippet OMZP::github
 zinit snippet OMZP::zsh-interactive-cd
